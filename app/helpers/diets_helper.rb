af2473e542ad2fd931aa4d29e97e4178e8f47e0b
mtrimner <matt.trimner@gmail.com>
2020-11-08 22:52:46 -0800

    created menu links and edited diet_helper

--- app/helpers/diets_helper.rb
+++ app/helpers/diets_helper.rb
@@ -1,6 +1,19 @@
 module DietsHelper 
     
-    def calories_per_meal(meals)
-        (daily_calories / meals)
+    def calories_per_meal
+        (daily_calories / current_user.diet.meals_per_day)
     end
+
+    def protein_amount
+        ((calories_per_meal * 0.25) / 4).floor
+    end
+
+    def fat_amount
+        ((calories_per_meal * 0.25) / 9).floor
+    end
+
+    def carbs_amount
+        ((calories_per_meal * 0.25) / 4).floor
+    end
+
 end
